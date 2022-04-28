70117840f26e00a2e07ef070f72c5b0bfdcaa962
amanchauhan71 <amanchauhan7172@gmail.com>
2022-04-28 17:28:51 +0530

    Project structure created

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Solving heart disease problem and creating monitoring or managing system",
+    long_description=long_description,
+    url="https://github.com/amanchauhan71/simple-dvc-project",
+    author_email="amanchauhan7172@gmail.com",
+    # package_dir={"":"src"},
+    # packages=find_packages(where="src"),
+    packages=["src"],
+    license="GNU",
+    python_requires=">=3.6",
+    install_requires= [
+        'dvc',
+        'dvc[gdrive]',
+        'dvc[s3]',
+        'pandas',
+        'scikit-learn'
+    ]
+)
